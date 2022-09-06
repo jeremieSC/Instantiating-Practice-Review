8762a8a417f45f39a291987d947ec1fd20772319
Jeremie Ledesma Smith <102314514+jeremieledesmasmith@users.noreply.github.com>
2022-09-06 22:14:58 +0000

    Initial commit

---
+++ Animals.java
@@ -0,0 +1,4 @@
+class Animals {
+  public static int lionWeight = 33;
+    public static String tiger = "Black stripes only";
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+    /*
+    Animals: Create a class called “Animals” and create two instances from it. Use two variables with the names of “lion” and “tiger”.
+    */
+      Animals animal = new Animals();
+    }
+}
